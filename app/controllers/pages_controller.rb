09e33d863f89299cad6a19ddc1e44c982b90e15e
xungngo <xungngo@gmail.com>
2017-07-10 22:00:47 -0400

    copied some files over from another app 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
   def home
-    session.delete(:authenticated_user) #also in application_controller.rb
-    #if user_signed_in? && current_user.active
     if current_user && current_user.active
       return redirect_to admin_dashboard_path
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # This hook allows the DRF to attach routes to the application's routes list.
+  # it should appear at the top of the routes.rb file.
+  DiditRailsFramework.setup_routes(self)
+
+  root 'pages#home', id: 'home'
+
+  # Other Pages
+  get '/home', to: redirect('/')
+
+  get 'admin/' => redirect('/')
+  get 'admin/dashboard' => 'admin#dashboard'
+
+  namespace :admin do
+    resources :locations
+    resources :users
+  end
+
 end
