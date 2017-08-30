d2f05ee633cc8b097dcc1c54c51e8d78f7f116b2
xungngo <xungngo@gmail.com>
2017-08-29 22:56:05 -0400

    added haversine formula to query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,10 @@ class PagesController < ApplicationController
   end
 
   def getmarkers
-    @markers = Marker.where("name LIKE (?)", "%#{params[:input_keyword]}%")
+    sql = "SELECT name, address, latitude, longitude FROM markers WHERE name LIKE '%#{params[:input_keyword]}%' GROUP BY name, address, latitude, longitude HAVING (3959 * acos(cos(radians('#{params[:input_latitude]}')) * cos(radians(latitude)) * cos(radians(longitude) - radians('#{params[:input_longitude]}')) + sin( radians('#{params[:input_latitude]}') ) * sin(radians(latitude)))) < 10"
+    @markers = ActiveRecord::Base.connection.execute(sql)
+    #@markers = @markers.where("name LIKE (?)", "%#{params[:input_keyword]}%")
+    #@markers = @markers.execute("HAVING distance < 5")
     render :json => @markers.as_json
   end
 end
