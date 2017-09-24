72f8f6c9719832593bfeee240e722bb1620f8a0a
xungngo <xungngo@gmail.com>
2017-09-24 18:16:45 -0400

    use variable for haversine in sql statement

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,14 @@ class PagesController < ApplicationController
   end
 
   def getmarkers
-    sql = "SELECT name, address, latitude, longitude FROM markers WHERE name LIKE '%#{params[:input_keyword]}%' GROUP BY name, address, latitude, longitude HAVING (3959 * acos(cos(radians('#{params[:input_latitude]}')) * cos(radians(latitude)) * cos(radians(longitude) - radians('#{params[:input_longitude]}')) + sin( radians('#{params[:input_latitude]}') ) * sin(radians(latitude)))) < 10"
+    haversine = "(3959 * acos(cos(radians('#{params[:input_latitude]}')) * cos(radians(latitude)) * cos(radians(longitude) - radians('#{params[:input_longitude]}')) + sin( radians('#{params[:input_latitude]}') ) * sin(radians(latitude))))"
+    sql = "SELECT name, address, latitude, longitude,
+          #{haversine} as distant
+    FROM markers WHERE name LIKE '%#{params[:input_keyword]}%' 
+    GROUP BY name, address, latitude, longitude 
+    HAVING #{haversine} < 10
+    ORDER BY distant ASC"
     @markers = ActiveRecord::Base.connection.execute(sql)
-    #@markers = @markers.where("name LIKE (?)", "%#{params[:input_keyword]}%")
-    #@markers = @markers.execute("HAVING distance < 5")
     render :json => @markers.as_json
   end
 end
