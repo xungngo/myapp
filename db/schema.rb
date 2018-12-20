2834dfd85e747beaf2328386bcb234d52916d28b
Xung Ngo <xungngo@gmail.com>
2018-12-19 22:02:29 -0500

    Remove halt_callback_chains_on_return_false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180627274511) do
+ActiveRecord::Schema.define(version: 2018_06_27_274511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180627274511) do
     t.integer "event_id"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.integer "image_file_size"
+    t.bigint "image_file_size"
     t.datetime "image_updated_at"
     t.integer "sort"
   end
@@ -164,7 +164,7 @@ ActiveRecord::Schema.define(version: 20180627274511) do
     t.datetime "security_updated_at"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
-    t.integer "avatar_file_size"
+    t.bigint "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "last_sign_in_at"
